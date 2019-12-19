948840cd5f77f0c307f419d824a86feb3269aa68
eslamharidy <eslamissy@hotmail.com>
2019-12-19 21:21:14 +0100

    animation basics

--- src/components/navbar/Contact.js
+++ src/components/navbar/Contact.js
@@ -9,22 +9,23 @@ import LinkedIn from './images/LinkedIn.png'
 export default class Contact extends Component {
     
     render() {
-        return <>
-        <Navbar bg="transperant">
-          <Navbar.Brand href="https://github.com/eslamharidy">Github</Navbar.Brand>
+        return <><div className="Navbar">
+        <Navbar bg="transperant" className="git">
+          <Navbar.Brand href="https://github.com/eslamharidy">
+          <span className="Github">Github</span>
           <img
               src={github}
               width="30"
               height="30"
               className="d-inline-block"
               alt="React Bootstrap logo"
-            />
+            /></Navbar.Brand>
          </Navbar>
         <br />
-        <Navbar bg="transperant">
+        <Navbar bg="transperant" className="linked">
           <Navbar.Brand href="https://www.linkedin.com/in/eslam-haridy-0b14316a/">
-          LinkedIn
-            <img
+           <span className="LinkedIn">LinkedIn</span>
+            <img id="Linkedin"
               src={LinkedIn}
               width="30"
               height="30"
@@ -34,17 +35,18 @@ export default class Contact extends Component {
           </Navbar.Brand>
         </Navbar>
         <br />
-        <Navbar bg="transperant" variant="light">
+        <Navbar bg="transperant"  variant="light" className="mail">
           <Navbar.Brand href="https://mail.google.com/mail/u/0/?view=cm&fs=1&tf=1&source=mailto&to=eslamissy@hotmail.com">
-          Email <img
-              alt=""
+          <span className="email">Email</span>
+           <img id="email"
+              alt="email"
               src={email}
               width="30"
               height="30"
               className="d-inline-block"
             />
           </Navbar.Brand>
-        </Navbar>
+        </Navbar></div>
       </>
                 
     }
