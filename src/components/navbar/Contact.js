dff639395880a0de2da9d5374c6392362344e140
eslamharidy <eslamissy@hotmail.com>
2019-12-19 18:28:09 +0100

    inline

--- src/components/navbar/Contact.js
+++ src/components/navbar/Contact.js
@@ -1,45 +1,48 @@
 import React, { Component } from 'react';
 import './Contact.css';
 import Navbar from 'react-bootstrap/Navbar';
+import github from './images/github.png'
+import email from './images/email.png'
+import LinkedIn from './images/LinkedIn.png'
 
 
 export default class Contact extends Component {
     
     render() {
         return <>
-        <Navbar bg="light">
+        <Navbar bg="transperant">
           <Navbar.Brand href="https://github.com/eslamharidy">Github</Navbar.Brand>
           <img
-              src="/logo.svg"
+              src={github}
               width="30"
               height="30"
-              className="d-inline-block align-top"
+              className="d-inline-block"
               alt="React Bootstrap logo"
             />
          </Navbar>
         <br />
-        <Navbar bg="dark">
+        <Navbar bg="transperant">
           <Navbar.Brand href="https://www.linkedin.com/in/eslam-haridy-0b14316a/">
+          LinkedIn
             <img
-              src="/logo.svg"
+              src={LinkedIn}
               width="30"
               height="30"
-              className="d-inline-block align-top"
+              className="d-inline-block"
               alt="React Bootstrap logo"
             />
-          </Navbar.Brand>LinkedIn
+          </Navbar.Brand>
         </Navbar>
         <br />
-        <Navbar bg="dark" variant="dark">
+        <Navbar bg="transperant" variant="light">
           <Navbar.Brand href="https://mail.google.com/mail/u/0/?view=cm&fs=1&tf=1&source=mailto&to=eslamissy@hotmail.com">
-            <img
+          Email <img
               alt=""
-              src="/logo.svg"
+              src={email}
               width="30"
               height="30"
-              className="d-inline-block align-top"
+              className="d-inline-block"
             />
-            Email
           </Navbar.Brand>
         </Navbar>
       </>
