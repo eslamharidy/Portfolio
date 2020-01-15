01097c9206220536de68aaf41fec66ad67d40a54
eslamharidy <eslamissy@hotmail.com>
2020-01-15 20:17:12 +0100

    about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,47 +5,46 @@ import './about.css';
 
 export default class About extends Component {
   render() {
-    return <div className="page">
+    return <div className="page" id="about">
       <div className="one">
-        
-      
-      <img alt="my face" className="face" 
-      data-aos="fade-right"
-      data-aos-offset="200"
-      data-aos-delay="50"
-      data-aos-duration="1000"
-      data-aos-easing="ease-in-out"
-      data-aos-mirror="true"
-      data-aos-once="false"
-      />
 
-        
-        <p
-        
-        data-aos="fade-down"
+        <div>
+          <img alt="my face" className="face"
+            data-aos="fade-right"
+            data-aos-offset="200"
+            data-aos-delay="50"
+            data-aos-duration="1000"
+            data-aos-easing="ease-in-out"
+            data-aos-mirror="true"
+            data-aos-once="false"
+          />
+          <p
+            data-aos="fade-down"
+            data-aos-offset="200"
+            data-aos-delay="50"
+            data-aos-duration="1000"
+            data-aos-easing="ease-in-out"
+            data-aos-mirror="true"
+            data-aos-once="false"
+          >I am Full stack developer with bachelor degree in business administration, always looking to learning new skills and taking new challenges.<br /><br />
+            Currently building Full stack Apps with <strong className="bold" id="js">JavaScript</strong>, React, and Redux on frontend.  Using Express.js ,Node.js, docker and PostgreSQL on backend. <br /><br />
+            Tomorrow new technologies will come, and i will always keep learning them!<br /><br />
+            Have you got a new idea? Lets build a webapp for it, <a id="contact" href="https://mail.google.com/mail/u/0/?view=cm&fs=1&tf=1&source=mailto&to=eslamissy@hotmail.com">
+              Contact me! </a>
+          </p></div>
+
+      </div>
+
+      <div
+        data-aos="fade-left"
         data-aos-offset="200"
         data-aos-delay="50"
         data-aos-duration="1000"
         data-aos-easing="ease-in-out"
         data-aos-mirror="true"
         data-aos-once="false"
-        >I am Full stack developer with bachelor degree in business administration, always looking to learning new skills and taking new challenges.<br/><br/>
-        Currently building Full stack Apps with <strong className="bold" id="js">JavaScript</strong>, React, and Redux on frontend.  Using Express.js ,Node.js, docker and PostgreSQL on backend. <br/><br/>
-        Tomorrow new technologies will come, and i will always keep learning them!<br/><br/>
-        Have you got a new idea? Lets build a webapp for it, <a id="contact" href="https://mail.google.com/mail/u/0/?view=cm&fs=1&tf=1&source=mailto&to=eslamissy@hotmail.com">
-          Contact me! </a> </p>
-</div>
-   
-      <div id="about" 
-      data-aos="fade-left"
-      data-aos-offset="200"
-      data-aos-delay="50"
-      data-aos-duration="1000"
-      data-aos-easing="ease-in-out"
-      data-aos-mirror="true"
-      data-aos-once="false"
-      data-aos-anchor-placement="top-center"
-      className="iframeWrapper">
+        data-aos-anchor-placement="top-center"
+        className="iframeWrapper">
         <h2 id="resumeCapt">Resume</h2>
         <iframe
           className="iframer"
@@ -62,6 +61,8 @@ export default class About extends Component {
         </div>
 
       </div>
+
+
 
     </div>
   }
